9fbf22e29b2e9043cf34278e3e4e184f9eeecaa8
Alex Nichol <alexnichol@comcast.net>
2012-04-02 13:51:07 -0400

    Initial Commit

---
+++ ANMachineAssembler/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ANMachineAssembler
+//
+//  Created by Alex Nichol on 4/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
